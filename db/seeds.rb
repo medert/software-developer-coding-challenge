156137ec86c5fa69fa0a088c98be456de4bccafb
Meder <m.toktosunov@icloud.com>
2020-03-01 17:17:08 -0500

    Seeded database with fake users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+    User.create(
+        first_name: FFaker::Name.first_name,
+        last_name: FFaker::Name.last_name,
+        email: FFaker::Internet.free_email,
+        admin: false
+    )
+end
