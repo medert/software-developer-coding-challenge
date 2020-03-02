c1bc603637acc3cfded68f1fa3c2417af8f096b4
Meder <m.toktosunov@icloud.com>
2020-03-01 23:40:30 -0500

    removed User model validations and relevant test suites so user can register with email field only

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
     describe 'validations' do
-        it { should have_valid(:first_name).when("Tom") }
-        it { should_not have_valid(:first_name).when("",nil) }
-
-        it {should have_valid(:last_name).when("Sawyer") }
-        it {should_not have_valid(:last_name).when("", nil) }
-        
         it {should have_valid(:email).when("toms@gmail.com") }
         it {should_not have_valid(:email).when("", nil, "Tom") }  
     end 
