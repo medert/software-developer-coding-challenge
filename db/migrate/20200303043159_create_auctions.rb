ed2ede82e3f8b599ff3a47096aed5651f646cbde
Meder <m.toktosunov@icloud.com>
2020-03-03 01:11:35 -0500

    Removed a 'end_date' field in Auction model

--- db/migrate/20200303043159_create_auctions.rb
+++ db/migrate/20200303043159_create_auctions.rb
@@ -3,7 +3,6 @@ class CreateAuctions < ActiveRecord::Migration[5.2]
     create_table :auctions do |t|
       t.string    :title
       t.string    :make
-      t.datetime  :end_date
       t.float     :current_price, default: 10.0
       t.integer   :user_id
 
