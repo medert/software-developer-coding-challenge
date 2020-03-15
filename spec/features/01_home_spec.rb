1431edc723b30f3477a6ea5a11e8514401536e1b
Meder <m.toktosunov@icloud.com>
2020-03-15 12:11:24 -0400

    Added home spec

--- spec/features/01_home_spec.rb
+++ spec/features/01_home_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature 'visit homepage' do
     scenario 'user sees slogan' do
         visit root_path
-        expect(page).to have_text("Place your first bid")
+        expect(page).to have_text("Place Your Winning Bid!")
     end
 end
