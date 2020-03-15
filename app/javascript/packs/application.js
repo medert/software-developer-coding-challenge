3336e87310b6a9dffa4ac696b79433ee7a0cf089
Meder <m.toktosunov@icloud.com>
2020-03-15 11:33:56 -0400

    Added JS to project

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,9 +14,3 @@
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-console.log('Hello World from Webpacker')
-
-import "bootstrap";
-import "../stylesheets/application";
-
