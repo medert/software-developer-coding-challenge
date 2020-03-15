18c320511bdcad59795c32c0718674f5dd1bef5d
Meder <m.toktosunov@icloud.com>
2020-03-15 12:05:59 -0400

    Modified user controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root 'home#index'
   devise_for :users
-  resources :users, only: [:show]
+  root to: 'home#index'
+  # resources :users, only: [:show]
 
   resources :auctions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2020_03_03_043159) do
   create_table "auctions", force: :cascade do |t|
     t.string "title"
     t.string "make"
-    t.datetime "end_date"
     t.float "current_price", default: 10.0
     t.integer "user_id"
     t.datetime "created_at", null: false
@@ -26,6 +25,9 @@ ActiveRecord::Schema.define(version: 2020_03_03_043159) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.string "admin", default: "f", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -38,9 +40,6 @@ ActiveRecord::Schema.define(version: 2020_03_03_043159) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
