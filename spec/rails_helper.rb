ee3d8619d57d484fa9f61ab2135350688f54c5fb
Meder <m.toktosunov@icloud.com>
2020-03-15 12:08:26 -0400

    loaded gems to rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,8 @@ require File.expand_path('../config/environment', __dir__)
 require 'rspec/rails'
 require 'capybara/rspec'
 require 'support/factory_bot'
+require 'support/devise'
+require 'support/database_cleaner'
 require 'shoulda'
 require 'shoulda-matchers'
 require 'valid_attribute'
@@ -44,7 +46,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
