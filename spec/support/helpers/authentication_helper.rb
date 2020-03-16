553ccbbe30e8b8a8426156ac4a08146171b22d35
Meder <m.toktosunov@icloud.com>
2020-03-15 20:01:56 -0400

    Added #sign_up_as

--- spec/support/helpers/authentication_helper.rb
+++ spec/support/helpers/authentication_helper.rb
@@ -5,4 +5,14 @@ module AuthenticationHelper
     fill_in 'Password', with: user.password
     click_button 'Log in'
   end
+
+  def sign_up_as(user)
+    visit new_user_registration_path
+    fill_in 'First Name', with: user.first_name
+    fill_in 'Last Name', with: user.last_name
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password confirmation', with: user.password_confirmation
+    click_button 'Sign up'
+  end
 end
