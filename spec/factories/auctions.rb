fa60f3327cb8517b3a2c6df35a293204acf07eb8
Meder <m.toktosunov@icloud.com>
2020-03-17 10:35:19 -0400

    Added sequencing to auctions factory

--- spec/factories/auctions.rb
+++ spec/factories/auctions.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :auction do
-    sequence(:title) {|n| "Subaru Forester no. #{n}!"}
-    sequence(:make)  {|n| "Subaru no. #{n}!"}
+    sequence(:title) {|n| "Bestno. #{n}"}
+    sequence(:make)  {|n| "Subaru no. #{n}"}
     current_price { 10.0 }
     association :user
   end
--- spec/features/auctions/index_spec.rb
+++ spec/features/auctions/index_spec.rb
@@ -6,19 +6,20 @@ require "rails_helper"
 # TO DO : As an authenticated user
 
 feature "authenticated user can view auctions" do
-    let(:user){create(:user)}
-    let(:auctions){create_list :auction, 3, user_id: user.id}
-
+    let!(:user){create(:user)}
+    let!(:auctions){create_list :auction, 2, user: user}
     scenario "user signs in and views auctions on root_path" do
         sign_in_as(user)
 
+        expect(page).to have_link("Auctions")
         expect(page).to have_content("Active Auctions")
         expect(page).to have_link("New Auction")
 
+
         auctions.each do |auction|
-            expect(page).to have_link(auction.title)
-            expect(page).to have_link(auction.make)
-            expect(page).to have_link(auction.current_price)
+            expect(page).to have_content(auction.title)
+            expect(page).to have_content(auction.make)
+            expect(page).to have_content(auction.current_price)
         end
     end
 end
