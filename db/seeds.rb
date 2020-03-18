d80768484f315b76fb8340bb9307c1857966d206
Meder <m.toktosunov@icloud.com>
2020-03-18 17:24:33 -0400

    removed space in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,6 @@
 # #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 # #   Character.create(name: 'Luke', movie: movies.first)
 
-# 10.times do
-#     User.create(
-#         first_name: FFaker::Name.first_name,
-#         last_name: FFaker::Name.last_name,
-#         email: FFaker::Internet.free_email,
-#         password: 'Sekret123',
-#         admin: false
-#     )
-# end
-
-# 20.times do
-#     Auction.create(
-#         title: FFaker::Product.product,
-#         make: FFaker::Product.product_name,
-#         curent_price: 10,
-#         user_id:
-#     )
-# end
-
-
 if Rails.env.development? || Rails.env.production?
   UserSeeder.seed!
   AuctionSeeder.seed!
