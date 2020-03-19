b7e422406219c2c269457ac1525baca002d5ec97
Meder <m.toktosunov@icloud.com>
2020-03-18 23:11:42 -0400

    Added Bid model associations with Auction and User models

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -2,7 +2,7 @@ class Auction < ApplicationRecord
     belongs_to :user, optional: true
 
     validates :title, presence: true
-    validates :make, presence: true
-    validates :current_price, presence: true
-    validates :user_id, presence: true
+    validates :make, presence: true, numericality: true
+    validates :current_price, presence: true, numericality: true
+    validates :user_id, presence: true, numericality: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   
   has_many  :auctions
+  has_many  :bids
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
