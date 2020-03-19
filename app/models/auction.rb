dcfe3297bcdd3148845db2ae9d0b838c298b17ad
Meder <m.toktosunov@icloud.com>
2020-03-18 23:52:37 -0400

    Corrected Auction model validation

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -2,7 +2,7 @@ class Auction < ApplicationRecord
     belongs_to :user, optional: true
 
     validates :title, presence: true
-    validates :make, presence: true, numericality: true
-    validates :current_price, presence: true, numericality: true
-    validates :user_id, presence: true, numericality: true
+    validates :make, presence: true
+    validates :current_price, presence: true
+    validates :user_id, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1}
 end
