65719b702131d9c7a302271e1c9cfbc16c8436dd
Meder <m.toktosunov@icloud.com>
2020-03-18 23:12:33 -0400

    Bid model db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_043159) do
+ActiveRecord::Schema.define(version: 2020_03_18_212755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2020_03_03_043159) do
     t.string "make"
     t.float "current_price", default: 10.0
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "bids", force: :cascade do |t|
+    t.float "amount"
+    t.integer "user_id"
+    t.integer "auction_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
