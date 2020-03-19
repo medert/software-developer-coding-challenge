12ed6fece919f950df0d4e943ff4c197b69025c2
Meder <m.toktosunov@icloud.com>
2020-03-19 00:29:25 -0400

    Added User model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   
-  has_many  :auctions
-  has_many  :bids
+  has_many  :auctions, dependent: :destroy
+  has_many  :bids, dependent: :destroy
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, presence: true
-  validates :email,    :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+  validates :email, presence: true, uniqueness: true,
+            :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i },
+            on: :create
 
 end
