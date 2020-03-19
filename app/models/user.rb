fead488f5cc0daa3622607ca451fbfb653b277ac
Meder <m.toktosunov@icloud.com>
2020-03-19 19:32:04 -0400

    Added additional User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true,
             :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i },
             on: :create
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
     describe 'validations' do
+        it {should have_valid(:first_name).when("John") }
+        it {should_not have_valid(:first_name).when("", nil) }
+
+        it {should have_valid(:last_name).when("Doe") }
+        it {should_not have_valid(:last_name).when("", nil) }
+
         it {should have_valid(:email).when("toms@gmail.com") }
-        it {should_not have_valid(:email).when("", nil, "Tom") }  
+        it {should_not have_valid(:email).when("", nil, "Tom") }
     end 
 end
