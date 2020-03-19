b56267036024e241258e2deaf2861269de2441a7
Meder <m.toktosunov@icloud.com>
2020-03-18 23:08:29 -0400

    Added more validation specs to Bid model

--- spec/models/bid_spec.rb
+++ spec/models/bid_spec.rb
@@ -1,19 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Bid, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
-
-
-RSpec.describe Bid, type: :model do
     describe 'validations' do
         it {should have_valid(:amount).when(1.00) }
-        it {should_not have_valid(:amount).when("", nil, "1.00") }
+        it {should_not have_valid(:amount).when("", nil) }
 
-        it {should have_valid(:user_id).when(1) }
-        it {should_not have_valid(:user_id).when("", nil, "1.00") }
+        it {should have_valid(:user_id).when(1,2,3,4) }
+        it {should_not have_valid(:user_id).when("", nil, "1.00", 0, -1) }
+        it {should_not have_valid(:user_id).when(0, -1) }
 
-        it {should have_valid(:auction_id).when(1) }
-        it {should_not have_valid(:auction_id).when("", nil, "1.00") }
+        it {should have_valid(:auction_id).when(1,2,3,4) }
+        it {should_not have_valid(:auction_id).when("", nil, "1.00", 0, -1) }
+        it {should_not have_valid(:auction_id).when(0, -1) }
     end
 end
