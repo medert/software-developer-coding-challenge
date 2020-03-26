9a5c1f03ef664c196b0fa749b83b2978aae9224a
Meder <m.toktosunov@icloud.com>
2020-03-26 16:39:51 -0400

    Cleaned up indentation - 2space tabs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
+  protected
 
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:update, keys: [:first_name, :last_name, :admin])
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :admin])
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:update, keys: [:first_name, :last_name, :admin])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :admin])
+  end
 end
