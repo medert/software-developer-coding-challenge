1762f15cb1dc25eb5c29e4480bd998be858b8b2e
Meder <m.toktosunov@icloud.com>
2020-03-26 16:30:44 -0400

    Modified auction table" " clear exit

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,9 +1,11 @@
 class Auction < ApplicationRecord
-    belongs_to  :user
-    has_many    :bids
+  belongs_to  :user
+  has_many    :bids
+  mount_uploader :image, ImageUploader
 
-    validates :title, presence: true
-    validates :make, presence: true
-    validates :current_price, presence: true
-    validates :user_id, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1}
+  validates :title, presence: true
+  validates :make, presence: true
+  validates :starting_price, presence: true
+  validates :realized_price, presence: true
+  validates :user_id, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1}
 end
--- db/migrate/20200303043159_create_auctions.rb
+++ db/migrate/20200303043159_create_auctions.rb
@@ -3,7 +3,8 @@ class CreateAuctions < ActiveRecord::Migration[5.2]
     create_table :auctions do |t|
       t.string    :title
       t.string    :make
-      t.float     :current_price, default: 10.0
+      t.float     :starting_price, default: 10.0
+      t.float     :realized_price, default: 0.00
       t.integer   :user_id
 
       t.timestamps
