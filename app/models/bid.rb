8c0c5b895023516b80b61129de01f59e4a87dbbf
Meder <m.toktosunov@icloud.com>
2020-03-26 16:19:39 -0400

    Added Bid model

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,9 +1,12 @@
 class Bid < ApplicationRecord
-    belongs_to :auction
-    belongs_to :user
+  belongs_to :auction
+  belongs_to :user
 
-    validates :amount, presence: true, numericality: true
-    validates :auction_id, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1}
-    validates :user_id, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1}
+  validates :amount, presence: true, numericality: {greater_than: :auction_realized_price}
+  validates :auction_id, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1}
+  validates :user_id, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1}
 
+  def auction_realized_price
+      auction.realized_price
+  end
 end
