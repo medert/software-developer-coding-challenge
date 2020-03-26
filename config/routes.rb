2a8150a55b99cf047d72a36090752b2f725b9051
Meder <m.toktosunov@icloud.com>
2020-03-26 16:22:00 -0400

    Configured nessted Bid resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
-  root to:    'home#index'
+  root to: 'home#index'
 
-  devise_for  :users
-  resources   :auctions
-  resources   :users, only: [:show]
-  resources   :bids
+  devise_for :users
+  resources :users, only: [:show]
+
+  resources :auctions do
+    resources :bids, only: [:create]
+  end
+
 end
