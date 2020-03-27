af79f328f61580662d5d28512087fd6e3231c34b
Meder <m.toktosunov@icloud.com>
2020-03-26 23:05:31 -0400

    Added more User model specs

--- spec/models/01_user_spec.rb
+++ spec/models/01_user_spec.rb
@@ -22,4 +22,18 @@ RSpec.describe User, type: :model do
     it {should have_valid(:email).when("toms@gmail.com") }
     it {should_not have_valid(:email).when("", nil, "Tom") }
   end
+
+  it "has none to begin with" do
+    expect(User.count).to eq(0)
+  end
+
+  it "has one after adding one" do
+    User.create(first_name: "John",last_name: "Doe",email: "free@gmail.com", password: "password")
+
+    expect(User.count).to eq(1)
+  end
+
+  it "has none after one was created in a previous example" do
+    expect(User.count).to eq(0)
+  end
 end
