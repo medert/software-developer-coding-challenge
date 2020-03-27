f0d24aaaffcda53bfb22b11350f59bf73de94a8e
Meder <m.toktosunov@icloud.com>
2020-03-26 23:05:02 -0400

    Added more Bid model specs

--- spec/models/03_bid_spec.rb
+++ spec/models/03_bid_spec.rb
@@ -23,4 +23,22 @@ RSpec.describe Bid, type: :model do
     it {should_not have_valid(:auction_id).when("", nil, "1.00", 0, -1) }
     it {should_not have_valid(:auction_id).when(0, -1) }
   end
+
+  let(:user){create(:user)}
+  let(:auction){create(:auction)}
+  before {login_as(user)}
+
+  it "has none to begin with" do
+    expect(Bid.count).to eq(0)
+  end
+
+  it "has one after adding one" do
+    Bid.create(amount: 300, auction: auction, user: user)
+
+    expect(Bid.count).to eq(1)
+  end
+
+  it "has none after one was created in a previous example" do
+    expect(Bid.count).to eq(0)
+  end
 end
